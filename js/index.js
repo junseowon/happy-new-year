f90c327a61fffd58dbfab18f759681a7ba686b6e
junseowon <junseowon@gmail.com>
2026-01-01 18:14:59 +0900

    feat: Happy new year

---
+++ js/index.js
@@ -0,0 +1,155 @@
+const canvas = document.getElementById('fireworksCanvas');
+const ctx = canvas.getContext('2d');
+
+let width, height;
+const fireworks = [];
+const particles = [];
+
+function resize() {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resize);
+resize();
+
+class Firework {
+    constructor(x, y, targetX, targetY) {
+        this.x = x;
+        this.y = y;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.distanceToTarget = Math.hypot(targetX - x, targetY - y);
+        this.distanceTraveled = 0;
+        this.coordinates = [];
+        this.coordinateCount = 3;
+        while (this.coordinateCount--) {
+            this.coordinates.push([this.x, this.y]);
+        }
+        this.angle = Math.atan2(targetY - y, targetX - x);
+        this.speed = 2;
+        this.acceleration = 1.05;
+        this.brightness = Math.random() * 50 + 50;
+        this.targetRadius = 1;
+    }
+
+    update(index) {
+        this.coordinates.pop();
+        this.coordinates.unshift([this.x, this.y]);
+
+        if (this.targetRadius < 8) {
+            this.targetRadius += 0.3;
+        } else {
+            this.targetRadius = 1;
+        }
+
+        this.speed *= this.acceleration;
+        const vx = Math.cos(this.angle) * this.speed;
+        const vy = Math.sin(this.angle) * this.speed;
+        this.distanceTraveled = Math.hypot(this.x + vx - this.x, this.y + vy - this.y);
+
+        if (Math.hypot(this.targetX - this.x, this.targetY - this.y) <= this.speed) {
+            createParticles(this.targetX, this.targetY);
+            fireworks.splice(index, 1);
+        } else {
+            this.x += vx;
+            this.y += vy;
+        }
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
+        ctx.lineTo(this.x, this.y);
+        ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, ${this.brightness}%)`;
+        ctx.stroke();
+    }
+}
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.coordinates = [];
+        this.coordinateCount = 5;
+        while (this.coordinateCount--) {
+            this.coordinates.push([this.x, this.y]);
+        }
+        this.angle = Math.random() * Math.PI * 2;
+        this.speed = Math.random() * 10 + 1;
+        this.friction = 0.95;
+        this.gravity = 1;
+        this.hue = Math.random() * 360;
+        this.brightness = Math.random() * 50 + 50;
+        this.alpha = 1;
+        this.decay = Math.random() * 0.03 + 0.01;
+    }
+
+    update(index) {
+        this.coordinates.pop();
+        this.coordinates.unshift([this.x, this.y]);
+        this.speed *= this.friction;
+        this.x += Math.cos(this.angle) * this.speed;
+        this.y += Math.sin(this.angle) * this.speed + this.gravity;
+        this.alpha -= this.decay;
+
+        if (this.alpha <= this.decay) {
+            particles.splice(index, 1);
+        }
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
+        ctx.lineTo(this.x, this.y);
+        ctx.strokeStyle = `hsla(${this.hue}, 100%, ${this.brightness}%, ${this.alpha})`;
+        ctx.stroke();
+    }
+}
+
+function createParticles(x, y) {
+    let particleCount = 30;
+    while (particleCount--) {
+        particles.push(new Particle(x, y));
+    }
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.fillRect(0, 0, width, height);
+    ctx.globalCompositeOperation = 'lighter';
+
+    let i = fireworks.length;
+    while (i--) {
+        fireworks[i].draw();
+        fireworks[i].update(i);
+    }
+
+    let j = particles.length;
+    while (j--) {
+        particles[j].draw();
+        particles[j].update(j);
+    }
+
+    if (Math.random() < 0.05) {
+        const x = Math.random() * width;
+        fireworks.push(new Firework(width / 2, height, x, Math.random() * height / 2));
+    }
+}
+
+function manualFirework() {
+    for(let i=0; i<3; i++) {
+        setTimeout(() => {
+            const x = Math.random() * width;
+            fireworks.push(new Firework(width / 2, height, x, Math.random() * height / 2));
+        }, i * 200);
+    }
+}
+
+window.addEventListener('mousedown', (e) => {
+    fireworks.push(new Firework(width / 2, height, e.clientX, e.clientY));
+});
+
+window.onload = loop;
